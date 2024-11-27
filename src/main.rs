3c9b6b82aec6b17f52eaa1f0031b03c87d6fc1c4
h3athen <gurungaryan73@gmail.com>
2024-11-27 02:02:22 -0800

    chore: add comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 mod gui;
 
 use eframe::{egui, NativeOptions};
